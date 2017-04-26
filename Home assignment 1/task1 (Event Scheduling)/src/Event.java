d56d9b4ad9863bebca1f3c75ade25b0e9cc34c35
NiclasLovdahl <niclas.lovdahl@gmail.com>
2017-04-26 16:59:35 +0200

    formating

--- Home assignment 1/task1 (Event Scheduling)/src/Event.java
+++ Home assignment 1/task1 (Event Scheduling)/src/Event.java
@@ -3,7 +3,7 @@
 // build a linked list which is used by the EventListClass. It would have been just as easy
 // to use a priority list which sorts events using eventTime.
 
-class Event{
+class Event {
 	public double eventTime;
 	public int eventType;
 	public Event next;

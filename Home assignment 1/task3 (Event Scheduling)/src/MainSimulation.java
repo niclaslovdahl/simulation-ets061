dc8b03c764423c529b3726dcda6ec627de2da941
NiclasLovdahl <niclas.lovdahl@gmail.com>
2017-04-27 18:26:07 +0200

    Mean customers in queuing network done. Time spent serving done.
    TODO: Measure time spent in queues.

---
+++ Home assignment 1/task3 (Event Scheduling)/src/MainSimulation.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class MainSimulation extends GlobalSimulation {
+
+	public static void simulate(double interarrival) {
+		Event actEvent;
+		State actState = new State(); // The state that shoud be used
+		actState.interarrival = interarrival; // Interarrival set to State
+												// class.
+		// Some events must be put in the event list at the beginning
+		insertEvent(ARRIVAL, 0);
+		insertEvent(MEASURE, 5);
+
+		// The main simulation loop
+		while (actState.noMeasurements < 1000) {
+			actEvent = eventList.fetchEvent();
+			time = actEvent.eventTime;
+			actState.treatEvent(actEvent);
+		}
+
+		// Printing the result of the simulation, in this case a mean value
+		System.out.println("---------- Interarrival time " + interarrival + " ----------");
+		System.out.println(
+				"Mean number of customers in queuing system: " + 1.0 * actState.accumulated / actState.noMeasurements);
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		simulate(2);
+		simulate(1.5);
+		simulate(1.1);
+	}
+}
---
+++ Home assignment 1/task3 (Event Scheduling)/src/State.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+class State extends GlobalSimulation {
+
+	// Here follows the state variables and other variables that might be needed
+	// e.g. for measurements
+	public int numberInQueue1 = 0, numberOfRejectedQueue1 = 0, numberOfArrivalsQueue1 = 0, numberInQueue2 = 0,
+			accumulated = 0, noMeasurements = 0;
+
+	Random slump = new Random(); // This is just a random number generator
+
+	public double interarrival = 0;
+	public double timeSpent = 0;
+
+	// The following method is called by the main program each time a new event
+	// has been fetched
+	// from the event list in the main loop.
+	public void treatEvent(Event x) {
+		switch (x.eventType) {
+		case ARRIVAL:
+			arrival();
+			break;
+		case READY:
+			ready();
+			break;
+		case ARRIVAL2:
+			arrival2();
+			break;
+		case READY2:
+			ready2();
+			break;
+		case MEASURE:
+			measure();
+			break;
+		}
+	}
+
+	// The following methods defines what should be done when an event takes
+	// place. This could
+	// have been placed in the case in treatEvent, but often it is simpler to
+	// write a method if
+	// things are getting more complicated than this.
+
+	private void arrival() {
+		numberOfArrivalsQueue1++;
+		if (numberInQueue1 == 0) {
+			double temp = expDist(1);
+			timeSpent += temp;
+			insertEvent(READY, time + temp);
+		}
+		insertEvent(ARRIVAL, time + expDist(interarrival));
+		numberInQueue1++;
+	}
+
+	private void ready() {
+		numberInQueue1--;
+		insertEvent(ARRIVAL2, time);
+		if (numberInQueue1 > 0) {
+			double temp = expDist(1);
+			timeSpent += temp;
+			insertEvent(READY, time + temp);
+		}
+
+	}
+
+	private void arrival2() {
+		if (numberInQueue2 == 0) {
+			double temp = expDist(1);
+			timeSpent += temp;
+			insertEvent(READY2, time + temp);
+		}
+		numberInQueue2++;
+	}
+
+	private void ready2() {
+		numberInQueue2--;
+		if (numberInQueue2 > 0) {
+			double temp = expDist(1);
+			timeSpent += temp;
+			insertEvent(READY2, time + temp);
+		}
+
+	}
+
+	private void measure() {
+		accumulated = accumulated + numberInQueue1 + numberInQueue2;
+		noMeasurements++;
+		insertEvent(MEASURE, time + expDist(5));
+	}
+
+	private double expDist(double mean) {
+		return -(mean) * Math.log(slump.nextDouble());
+	}
+}

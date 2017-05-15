6dec0aa030a1537c1e115b4d68021b426d3cb1c3
Niclas Lovdahl <niclas.lovdahl@gmail.com>
2017-05-15 16:35:54 +0200

    LP 1b done.

--- Home assignment 2/LP_1b.m
+++ Home assignment 2/LP_1b.m
@@ -33,4 +33,4 @@ A = [2 0 0 0 0;
  %options = optimoptions('linprog', 'Algorithm', 'interior-point', 'Display', 'off');
  %options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'iter');
  options = optimoptions('linprog', 'Algorithm', 'dual-simplex', 'Display', 'off');
- [x,fval,exitflag,output,lambda] = linprog(c', A, b, [], [], lb, [], [], options)
+ [x,fval,exitflag,output,lambda] = linprog(c, A, b, [], [], lb, [], [], options)
